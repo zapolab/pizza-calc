48008f6269e96d806ffd868aba4a47ea499babf3
tisatoelia <devele14@gmail.com>
2026-08-13 10:02:19 +0200

    feat: use clamped values

--- src/lib/dough.ts
+++ src/lib/dough.ts
@@ -1,4 +1,4 @@
-import type { PresetValues } from './presets';
+import { clampValues, type PresetValues } from './presets';
 
 export type Results = {
 	flour: number;
@@ -16,7 +16,7 @@ function round(num: number, fractionDigits: number): number {
 }
 
 export function computeResults(values: PresetValues): Results {
-  const {
+	const {
 		doughBallCount,
 		doughBallWeight,
 		hydration,
@@ -26,25 +26,25 @@ export function computeResults(values: PresetValues): Results {
 		oilPerLiter,
 		roomTemperature,
 		panPizza
-  } = values;
+	} = clampValues(values);
 
-	const normTemp  = roomTemperature * (1 - 0.25 * (panPizza ? 1 : 0));
+	const normTemp = roomTemperature * (1 - 0.25 * (panPizza ? 1 : 0));
 	const normHours = proofingHours - 0.9 * Math.min(fridgeHours, proofingHours - 1);
 	const yeastRatio =
-  (2250 * (1 + saltPerLiter / 200) * (1 + oilPerLiter / 300)) /
-  ((4.2 * hydration - 80 - 0.0305 * Math.pow(hydration, 2)) *
-    Math.pow(normTemp, 2.5) *
-    Math.pow(normHours, 1.2));
+		(2250 * (1 + saltPerLiter / 200) * (1 + oilPerLiter / 300)) /
+		((4.2 * hydration - 80 - 0.0305 * Math.pow(hydration, 2)) *
+			Math.pow(normTemp, 2.5) *
+			Math.pow(normHours, 1.2));
 	const totalWeight = doughBallCount * doughBallWeight;
 
-	const flourExact = totalWeight /
-  (1 + (hydration / 100) * (1 + (saltPerLiter + oilPerLiter) / 1000));
+	const flourExact =
+		totalWeight / (1 + (hydration / 100) * (1 + (saltPerLiter + oilPerLiter) / 1000));
 	const waterExact = (hydration / 100) * flourExact;
 
 	const flour = Math.round(flourExact);
 	const water = Math.round(waterExact);
-	const salt  = Math.round((saltPerLiter / 1000) * waterExact);
-	const oil   = Math.round((oilPerLiter  / 1000) * waterExact);
+	const salt = Math.round((saltPerLiter / 1000) * waterExact);
+	const oil = Math.round((oilPerLiter / 1000) * waterExact);
 
 	const wetYeast = round(flourExact * yeastRatio, 2);
 	const dryYeast = round(wetYeast * DRY_YEAST_FACTOR, 2);
