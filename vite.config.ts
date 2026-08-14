5104f821fae123c1f4ca98078bd3e9d54b6d7a1d
Elia <devele14@gmail.com>
2026-08-14 19:06:56 +0200

    Feat/multi flour dough (#7)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import tailwindcss from '@tailwindcss/vite';
 import adapter from '@sveltejs/adapter-node';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [
@@ -19,5 +19,9 @@ export default defineConfig({
 				}
 			}
 		})
-	]
+	],
+	test: {
+		include: ['src/**/*.test.ts'],
+		environment: 'node'
+	}
 });
