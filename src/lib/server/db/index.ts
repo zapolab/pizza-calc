32eca87324996a8c6b9cbdeb2489fd8d7aaa67bb
tisatoelia <devele14@gmail.com>
2026-08-16 18:08:08 +0200

    fix: was failing because `DATABASE_URL` is not set while building

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,9 +3,16 @@ import Database from 'better-sqlite3';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+type Db = ReturnType<typeof drizzle<typeof schema>>;
 
-const client = new Database(env.DATABASE_URL);
-client.pragma('foreign_keys = ON');
+let instance: Db | undefined;
 
-export const db = drizzle(client, { schema });
+export function getDb(): Db {
+	if (instance) return instance;
+	if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
+	const client = new Database(env.DATABASE_URL);
+	client.pragma('foreign_keys = ON');
+	instance = drizzle(client, { schema });
+	return instance;
+}
