6151eea2ec203a8b70948a8280603ea201416fba
tisatoelia <devele14@gmail.com>
2026-08-16 17:59:42 +0200

    feat: `isDefault` attribute on `preset` table

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -3,13 +3,14 @@ import { integer, primaryKey, sqliteTable, text } from 'drizzle-orm/sqlite-core'
 export const flourType = sqliteTable('flour_type', {
 	id: integer().primaryKey({ autoIncrement: true }),
 	name: text().notNull(),
-	/** Display order of the catalog */
+	/** Display order of the catalog (future use)*/
 	sortOrder: integer().notNull()
 });
 
 export const preset = sqliteTable('preset', {
 	id: integer().primaryKey({ autoIncrement: true }),
 	name: text().notNull(),
+	isDefault: integer({ mode: 'boolean' }).notNull().default(false),
 	doughBallCount: integer().notNull(),
 	doughBallWeight: integer().notNull(),
 	hydration: integer().notNull(),
