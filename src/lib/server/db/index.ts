4eb973cee244c3f03c57c0058273c22ff0ff963e
tisatoelia <devele14@gmail.com>
2026-08-16 16:16:24 +0200

    fix: enable foreign keys

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -6,5 +6,6 @@ import { env } from '$env/dynamic/private';
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
 const client = new Database(env.DATABASE_URL);
+client.pragma('foreign_keys = ON');
 
 export const db = drizzle(client, { schema });
